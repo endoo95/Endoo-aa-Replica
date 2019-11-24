c9131509148bdf339f088b266ee5bea018cf0e29
endoo-laptop <endoo95@gmail.com>
2019-11-24 13:51:28 +0100

    Wielki powrót!
    Naprawa błędów. Porpawienie gry do stanu "grywalności".
    
    TODO:
    Meneger audio (zwykłe nie wystarczy).
    Efekty dźwiękowe.
    Zrobić ten HigScore na telefon.

---
+++ Unity Project/Assets/Scripts/MainThemeScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainThemeScript : MonoBehaviour
+{
+    public GameObject mainTheme;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Debug.Log("Niszczenie");
+        }
+        else
+        {
+            Debug.Log("ELSE");
+        }
+    }
+}
